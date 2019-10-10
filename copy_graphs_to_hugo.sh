a8892cee1cca862954ab871a2c5526aaaa9ae951
Josh Cook <joshuacook0023@gmail.com>
2019-10-10 00:07:16 -0400

    build notebook: Thu Oct 10 00:07:16 EDT 2019

--- copy_graphs_to_hugo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Copy the graphs from the analysis to the "reports/static/img/"
-# to be used in the notebook.
-
-cp -r graphs reports/static/img
--- make_notebook.sh
+++ make_notebook.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# Run to build, commit, and push notebook
+
+## Copy the graphs from the analysis to the "reports/static/img/"
+## to be used in the notebook.
+
+cp -r graphs reports/static/img
+
+
+## Build hugo website
 
 cd reports/
 
 hugo -d ../docs
 
 cd ..
+
+
+## Commit to repo
 
 git add -A
 
