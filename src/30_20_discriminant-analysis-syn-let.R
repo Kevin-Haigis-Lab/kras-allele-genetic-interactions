45b6e78585928fda26ca903bdeb20047686535ab
Josh Cook <joshuacook0023@gmail.com>
2019-10-31 12:51:58 -0400

    feat: LDA for alleles in DepMap

--- src/30_20_discriminant-analysis-syn-let.R
+++ src/30_20_discriminant-analysis-syn-let.R
@@ -4,14 +4,14 @@
 
 
 caclulate_proportions <- function(lda_model, cancer) {
-    (lda_model$svd^2) / sum(lda_model$svd^2)
+    (lda_model$svd ^ 2) / sum(lda_model$svd ^ 2)
 }
 
 # LASSO-penalized logistic regression on WT vs mutant KRAS
-lda_wrapper <- function(df) {
+lda_wrapper <- function(cancer, data, ...) {
     set.seed(0)
 
-    data <- df %>%
+    mod_data <- data %>%
         select(allele, dep_map_id, hugo_symbol, gene_effect) %>%
         unique() %>%
         group_by(hugo_symbol) %>%
@@ -22,18 +22,19 @@ lda_wrapper <- function(df) {
                     values_from = gene_effect) %>%
         select(-dep_map_id)
 
-    data[, -1] <- as.data.frame(data[, -1]) %>%
+    mod_data[, -1] <- as.data.frame(mod_data[, -1]) %>%
         DMwR::knnImputation(k = 5, scale = FALSE) %>%
         as_tibble()
 
-    fit_lda <- MASS::lda(allele ~ ., data = data)
-    proportions <- caclulate_proportions(fit_lda) * 100 %>% round(1)
+    fit_lda <- MASS::lda(allele ~ ., data = mod_data)
+    proportions <- round(caclulate_proportions(fit_lda) * 100, 1)
 
-    p <- predict(object = fit_lda, newdata = data)$x %>%
+    p <- predict(object = fit_lda, newdata = mod_data)$x %>%
         as_tibble() %>%
-        mutate(allele = data$allele) %>%
+        mutate(allele = mod_data$allele) %>%
         ggplot(aes(x = LD1, y = LD2)) +
         geom_point(aes(color = allele)) +
+        scale_color_manual(values = short_allele_pal) +
         theme_bw() +
         theme(
             plot.title = element_text(hjust = 0.5)
@@ -47,7 +48,7 @@ lda_wrapper <- function(df) {
 
     save_path <- plot_path("30_20_discriminant-analysis-syn-let",
                            glue("{cancer}_lda.svg"))
-    ggsave_wrapper(p, save_path, medium)
+    ggsave_wrapper(p, save_path, "medium")
 
     invisible(fit_lda)
 }
@@ -55,12 +56,11 @@ lda_wrapper <- function(df) {
 # Run a LASSO-penalized logistic regression on WT vs. mutant KRAS
 #   for each cancer.
 model_data %>%
+    filter(!cancer %in% c("SKCM", "MM")) %>%
     group_by(cancer) %>%
     nest() %>%
-    mutate(lasso_log_model = map2(data, cancer, lda_wrapper))
+    mutate(fit_lda = purrr::map2(cancer, data, lda_wrapper))
 
 
 # Finished the analysis, but it doesn't show anything interesting.
 # Documented on 10/28/2019
-
-# TODO: finish up MVP and comment in notebook
