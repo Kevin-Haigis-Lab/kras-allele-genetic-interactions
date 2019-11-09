d14ecf6062228176b8482dcba121ffbef2b1104b
Josh Cook <joshuacook0023@gmail.com>
2019-11-09 16:30:08 -0500

    feat: plot color palettes (better than `scales::show_cols()`)

--- lib/helpers.R
+++ lib/helpers.R
@@ -1,4 +1,6 @@
 
+#### ---- Globally relevant functions ---- ####
+
 #' Get Hugo identifiers from the DepMap combined name format
 #'
 #' Extracts the Hugo gene identifiers from a vector of values with the format:
@@ -23,6 +25,8 @@ get_entrez_from_depmap_ids <- function(x, convert_to_num = FALSE) {
 
 
 
+#### ---- Helpful regular expressions ---- ####
+
 # helpful data for identifying KRAS mutations
 kras_hotspot_codons <- list(num = c(12, 13, 61, 117, 146))
 kras_hotspot_codons$char <- as.character(kras_hotspot_codons$num)
@@ -31,6 +35,59 @@ kras_hotspot_codons$regex <- paste0(kras_hotspot_codons$char, collapse = "|")
 # cancer regex
 cancer_regex <- "COAD|LUAD|PAAD|MM|SKCM"
 
+
+
+#### ---- Helpful colors/palettes ---- ####
+
+
+# Make a tile plot for a palette of colors.
+# Is simillar to `scales::show_col()` except this returns a `ggplot2` object.
+show_palette <- function(cols,
+                         num_rows = "square",
+                         label_size = 5,
+                         base_size = label_size) {
+
+    if (is.null(names(cols))) {
+        col_names <- cols
+    } else {
+        col_names <- names(cols)
+    }
+
+    if (num_rows == "square") {
+        num_rows <- ceiling(sqrt(length(cols)))
+    } else if (!is.numeric(num_rows)) {
+        stop("The `num_rows` argument must be either 'smart' or a numeric value.")
+    }
+    num_cols <- ceiling(length(cols) / num_rows)
+
+    col_assignments <- c()
+    for (j in seq(num_rows, 1)) {
+        col_assignments <- c(col_assignments, rep(j, num_cols))
+    }
+    row_assignments <- rep(seq(1, num_cols), num_rows)
+
+    tibble(col_names = col_names,
+           color_vals = cols,
+           x = row_assignments[1:length(cols)],
+           y = col_assignments[1:length(cols)]) %>%
+        ggplot(aes(x = x, y = y)) +
+        geom_tile(aes(fill = color_vals), color = NA) +
+        geom_text(aes(label = col_names), size = label_size, family = "Arial") +
+        scale_fill_identity() +
+        scale_x_discrete(expand = c(0, 0)) +
+        scale_y_discrete(expand = c(0, 0)) +
+        theme_bw(
+            base_family = "Arial",
+            base_size = base_size
+        ) +
+        theme(
+            axis.text = element_blank(),
+            axis.title = element_blank(),
+            axis.ticks = element_blank(),
+        )
+}
+
+
 # coding mutation variant classification terms
 coding_mut_var_classes <- c(
     "De_novo_Start_OutOfFrame", "Frame_Shift_Del", "Frame_Shift_Ins",
@@ -38,7 +95,6 @@ coding_mut_var_classes <- c(
     "Nonstop_Mutation", "Splice_Site", "Start_Codon_Del", "Start_Codon_Ins",
     "Start_Codon_SNP", "Stop_Codon_Del", "Stop_Codon_Ins"
 )
-
 
 mapping_mutation_types_to_human_readable <- list(
     "missense_mutation" = "missense mutation",
@@ -80,18 +136,18 @@ mapping_mutation_types_to_human_readable <- list(
 
 
 ## make color palette: does NOT run on O2, but does on Mac
-s <- 0
-while(TRUE) {
-    cat("Using", s, "as the seed value. ")
-    set.seed(s)
-    unique_muts <- unique(unname(unlist(mapping_mutation_types_to_human_readable)))
-    n <- length(unique_muts)
-    cols <- randomcoloR::randomColor(n, luminosity = "light")
-    scales::show_col(cols)
-    a <- readline(prompt = "Press Enter to continue... ")
-    if (a == "s") { break }
-    s <- s + 1
-}
+# s <- 0
+# while(TRUE) {
+#     cat("Using", s, "as the seed value. ")
+#     set.seed(s)
+#     unique_muts <- unique(unname(unlist(mapping_mutation_types_to_human_readable)))
+#     n <- length(unique_muts)
+#     cols <- randomcoloR::randomColor(n, luminosity = "light")
+#     scales::show_col(cols)
+#     a <- readline(prompt = "Press Enter to continue... ")
+#     if (a == "s") { break }
+#     s <- s + 1
+# }
 
 set.seed(30)
 unique_muts <- unique(unname(unlist(mapping_mutation_types_to_human_readable)))
@@ -115,6 +171,11 @@ mutation_pal <- list(
 )
 names(mutation_pal) <- unique(unname(unlist(mapping_mutation_types_to_human_readable)))
 
+ggsave_wrapper(
+    show_palette(mutation_pal, "square", label_size = 4),
+    plot_path("00_miscellaneous", "mutation_pal.svg"),
+    width = 12, height = 4
+)
 
 
 # colors for KRAS alleles
@@ -140,6 +201,12 @@ allele_palette <- c(allele_palette, "WT" = "grey50")
 short_allele_pal <- allele_palette
 names(short_allele_pal) <- str_remove_all(names(short_allele_pal), "KRAS ")
 
+ggsave_wrapper(
+    show_palette(short_allele_pal, "square"),
+    plot_path("00_miscellaneous", "short_alleles_pal.svg"),
+    "small"
+)
+
 
 # colors for cancers
 cancer_palette <- c(
@@ -150,9 +217,28 @@ cancer_palette <- c(
     SKCM = "darksalmon"
 )
 
+ggsave_wrapper(
+    show_palette(cancer_palette, num_rows = 1),
+    plot_path("00_miscellaneous", "cancer_palette.svg"),
+    width = 6, height = 0.3
+)
+
+
 
 # colors for comutation vs mutual exclusivity
 comut_mutex_pal <- c(
-    comutation = "lightskyblue",
-    exclusivity = "palegreen3"
+    comutation = "#18BCF2",  # blue
+    exclusivity = "#19C462"  # green
+)
+
+# Colors for extremes of synthetic lethal values.
+synthetic_lethal_pal <- c(
+    up = "#F47747",   # orange
+    down = "#A33BD5"  # purple
+)
+
+ggsave_wrapper(
+    show_palette(c(comut_mutex_pal, synthetic_lethal_pal)),
+    plot_path("00_miscellaneous", "genetic_interaction_pal.svg"),
+    width = 3.5, height = 2
 )
