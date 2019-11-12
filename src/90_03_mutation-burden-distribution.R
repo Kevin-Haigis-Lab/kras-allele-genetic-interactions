7ab191a5b5e4fee9ebccffb2749af6906eacaad1
Josh Cook <joshuacook0023@gmail.com>
2019-11-12 09:24:31 -0500

    styling changes

--- src/90_03_mutation-burden-distribution.R
+++ src/90_03_mutation-burden-distribution.R
@@ -9,8 +9,8 @@ plot_distribution_of_mutation_count <- function(cancer, data, save_name) {
         ggplot(aes(x = tumor_sample_barcode, y = log10(n))) +
         facet_grid(~ dataset_label, scales = "free_x") +
         geom_point(aes(color = log10(n), shape = is_hypermutant), size = 0.1) +
-        scale_color_viridis_c() +
-        scale_shape_manual(values = c(20, 4)) +
+        scale_color_viridis_c(guide = FALSE) +
+        scale_shape_manual(values = c(20, 4), guide = FALSE) +
         theme_bw(base_size = 8, base_family = "arial") +
         theme(
             plot.title = element_text(hjust = 0.5),
@@ -25,7 +25,7 @@ plot_distribution_of_mutation_count <- function(cancer, data, save_name) {
             y = "log10( num. mutations )"
         )
     save_path <- plot_path("90_03_mutation-burden-distribution", save_name)
-    ggsave_wrapper(p, save_path, "wide")
+    ggsave_wrapper(p, save_path, width = 8, height = 2.5)
 }
 
 # Distribution of all mutations per sample.
@@ -65,23 +65,23 @@ plot_distribution_of_mutation_type <- function(cancer, data, save_name) {
         mutate(dataset_label = paste0(dataset, "\n", target),
                mutation_type_hr = factor(mutation_type_hr, levels = mutation_type_hr_order)) %>%
         ggplot(aes(x = dataset_label, y = n)) +
-        geom_col(aes(fill = mutation_type_hr), position = "fill", color = "black") +
+        geom_col(aes(fill = mutation_type_hr), position = "fill") +
         scale_y_continuous(expand = c(0, 0)) +
         scale_fill_manual(values = mutation_pal) +
         theme_bw(base_size = 8, base_family = "arial") +
         theme(
             plot.title = element_text(hjust = 0.5),
             axis.text.x = element_text(angle = 40, hjust = 1.0),
+            axis.title.x = element_blank(),
             panel.grid.major.x = element_blank(),
         ) +
         labs(
             title = glue("Distribution of mutations types in {cancer} samples"),
-            x = "data source",
             y = "number of mutations detected",
             fill = "mutation type"
         )
     save_path <- plot_path("90_03_mutation-burden-distribution", save_name)
-    ggsave_wrapper(p, save_path, "medium")
+    ggsave_wrapper(p, save_path, "small")
 }
 
 
