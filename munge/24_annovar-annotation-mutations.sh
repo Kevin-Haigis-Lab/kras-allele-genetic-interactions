612bc33539fd000deacf3e7dca5d121b7ce1adbc
Josh Cook <joshuacook0023@gmail.com>
2019-11-19 00:19:56 -0500

    run annovar

---
+++ munge/24_annovar-annotation-mutations.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH -p priority
+#SBATCH -c 1
+#SBATCH -t 0-6:00
+#SBATCH --mem 8G
+#SBATCH --mail-type NONE
+#SBATCH -o logs/annovar/24_annovar-annotation-mutations.log
+#SBATCH -e annovar_run.log
+
+
+module load gcc annovar/20170601
+
+ANNOVAR_PATH=/n/app/annovar/20170601
+
+$ANNOVAR_PATH/table_annovar.pl \
+	$1 \
+	$ANNOVAR_PATH/humandb \
+	--protocol refGene,dbnsfp30a,clinvar_20160302,cosmic68wgs,icgc21 \
+	--operation g,f,f,f,f \
+	--verbose \
+	--buildver hg19 \
+	--remove \
+	--nastring NA \
+	--otherinfo \
+	-out $2
+
+echo "ANNOVAR FINISHED"
