1f2bd87cc6c7f437511f9c8d9a2445d358dd24e7
Josh Cook <joshuacook0023@gmail.com>
2019-12-05 14:11:14 -0500

    add descriptions for mutational signatures from GM

--- munge/55_mutational-signatures.R
+++ munge/55_mutational-signatures.R
@@ -1,5 +1,6 @@
 # Parse the mutational signatures results from GM.
 
+
 ProjectTemplate::cache("mutational_signatures_df",
 {
     # KRAS and tumor sample information.
@@ -22,15 +23,23 @@ ProjectTemplate::cache("mutational_signatures_df",
         pivot_longer(-tumor_sample_barcode,
                      names_to = "signature",
                      values_to = "contribution") %>%
-        mutate(signature = str_remove(signature, "Signature\\.")) %>%
+        mutate(
+            signature = str_remove(signature, "Signature\\."),
+            signature = str_to_upper(signature)
+        ) %>%
         left_join(kras_muts, by = "tumor_sample_barcode") %>%
-        filter(!is_hypermutant & cancer != "SKCM")
+        filter(!is_hypermutant & cancer != "SKCM") %>%
+        left_join(signature_description_df, by = "signature")
 
     # Check no missing data for KRAS allele.
     if (any(is.na(mutational_signatures_df$ras_allele))) {
         stop("There are `NA` values for KRAS allele.")
     }
 
+    # Check no missing signature descriptions.
+    if (any(is.na(mutational_signatures_df$description))) {
+        stop("Not all mut. sigs. have descriptions.")
+    }
 
     log_rows(logger, mutational_signatures_df, "mutational_signatures_df")
     info(logger, "Caching `mutational_signatures_df`.")
