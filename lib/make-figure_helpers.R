648cbca163f83b242ce4165749ae661ed5f42fa7
Josh Cook <joshuacook0023@gmail.com>
2019-12-08 16:00:54 -0500

    retrieve figure dimensions; automatically use latest version of a figure

--- lib/make-figure_helpers.R
+++ lib/make-figure_helpers.R
@@ -1,11 +1,86 @@
 
+#### ---- Figure dimensions ---- ####
+
+# Data sourced from NRJ guidelines.
+FIG_WIDTHS <- c(88, 180)
+FIG_HEIGHTS <- tibble::tribble(
+    ~n_col, ~height_class, ~height, ~max_cation_wc,
+         1,       "short",     130,            300,
+         1,      "medium",     180,            150,
+         1,        "tall",     220,             50,
+         2,       "short",     185,            300,
+         2,      "medium",     210,            150,
+         2,        "tall",     225,             50
+) %>%
+    mutate(
+        height_class_short = str_sub(height_class, 1, 1),
+        height_class_num = rep(seq(1, 3), 2)
+    )
+
+#' Returns the figure dimensions (width and height) in "mm".
+get_figure_dimensions <- function(n_col = 1, height_class = "medium") {
+
+    width <- FIG_WIDTHS[[n_col]]
+    height <- FIG_HEIGHTS %>%
+        filter(n_col == !!n_col) %>%
+        filter(
+            height_class == !!height_class |
+            height_class_short == !!height_class |
+            height_class_num == !!height_class
+        )
+
+    if (nrow(height) == 0) {
+        stop(glue("Height class '{height_class}' is not supported."))
+    }
+
+    return(list(
+        width = width,
+        height = height$height
+    ))
+}
+
+
+
+#### ---- Saving and retrieving figure protos ---- ####
+
 # The root directory for the protos.
 FIGURE_PROTOS_DIR <- file.path("paper", "figures", "figure_protos")
 
-#' Get the path to proto file for version `version` of Figure `figure_num`.
-get_fig_proto_path <- function(name, figure_num, version = 1) {
-    fig_dir <- glue("figure_{figure_num}-{figure_num}")
-    full_name <- glue("{name}.rds")
-    file.path(FIGURE_PROTOS_DIR, fig_dir, name)
+#' Returns the numeric value for the lowest subversion of Figure `figure_num`.
+get_latest_version_of_figure <- function(figure_num) {
+    all_dirs <- list.dirs(FIGURE_PROTOS_DIR,
+                          full.names = FALSE,
+                          recursive = FALSE)
+    idx <- str_detect(all_dirs, glue("_{figure_num}-"))
+    all_dirs <- all_dirs[idx]
+    subversions <- str_split_fixed(all_dirs, "_|-", 3)[, 3] %>%
+        unlist() %>%
+        as.numeric()
+    return(max(subversions))
 }
 
+
+#' Get the path to proto file for version `version` of Figure `figure_num`.
+#' The general format is "figure_protos/figure_00-000/filename.rds".
+get_fig_proto_path <- function(name, figure_num, version = "latest") {
+
+    base_n <- str_pad(as.character(figure_num), 2, pad = "0")
+    if (version == "latest") {
+        version <- get_latest_version_of_figure(base_n)
+    }
+    sub_n <- str_pad(as.character(version), 3, pad = "0")
+
+
+    fig_dir <- glue("figure_{base_n}-{sub_n}")
+    full_name <- glue("{name}.rds")
+
+    file.path(FIGURE_PROTOS_DIR, fig_dir, full_name)
+}
+
+
+#' Read in the proto RDS file names `name` for version `version` of
+#' Figure `figure_num`.
+read_fig_proto <- function(name, figure_num, version = "latest") {
+    path <- get_fig_proto_path(name, figure_num, version)
+    readRDS(path)
+}
