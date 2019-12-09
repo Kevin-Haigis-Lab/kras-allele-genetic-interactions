da0d70aa51aac816cd9c4fa3a47b1e80513ac702
Josh Cook <joshuacook0023@gmail.com>
2019-12-09 16:17:40 -0500

    remove unnecessary plots

--- src/20_40_highlivel-genetic-interactions.R
+++ src/20_40_highlivel-genetic-interactions.R
@@ -1,9 +1,10 @@
 
+# Analyze the comutation interaction networks at a high level.
+
+GRAPHS_DIR <- "20_40_highlivel-genetic-interactions"
+reset_graph_directory(GRAPHS_DIR)
+
 #### ---- UpSetR plot for similarity of alleles ---- ####
-
-info(logger, "Making UpSet plots for each cancer and genetic interactions.")
-
-library(UpSetR)
 
 # Make UpSetR plots for data nested by `cancer` and `genetic_interaction`
 allele_gene_interaction_upsetr <- function(cancer, genetic_interaction, data) {
@@ -12,7 +13,7 @@ allele_gene_interaction_upsetr <- function(cancer, genetic_interaction, data) {
         group_by(allele) %>%
         summarise(genes = list(hugo_symbol)) %>%
         deframe()
-    save_path <- plot_path("20_40_highlivel-genetic-interactions",
+    save_path <- plot_path(GRAPHS_DIR,
                            glue("{cancer}_{genetic_interaction}_upset.svg"))
 
     sizes <- c(8, 6)
@@ -24,12 +25,21 @@ allele_gene_interaction_upsetr <- function(cancer, genetic_interaction, data) {
     dev.off()
 }
 
-genetic_interaction_df %>%
-    select(cancer, allele, hugo_symbol, genetic_interaction) %>%
-    unique() %>%
-    group_by(cancer, genetic_interaction) %>%
-    nest() %>%
-    pwalk(allele_gene_interaction_upsetr)
+caps <- as.list(capabilities())
+if (caps$X11) {
+    info(logger, "Making UpSet plots for each cancer and genetic interactions.")
+
+    library(UpSetR)
+
+    genetic_interaction_df %>%
+        select(cancer, allele, hugo_symbol, genetic_interaction) %>%
+        unique() %>%
+        group_by(cancer, genetic_interaction) %>%
+        nest() %>%
+        pwalk(allele_gene_interaction_upsetr)
+}
+
+
 
 
 
@@ -50,68 +60,47 @@ ProjectTemplate::cache("genetic_interaction_gr",
     return(genetic_interaction_gr)
 })
 
-set.seed(0)
-for (CANCER in unique(genetic_interaction_df$cancer)) {
-    gr_plot <- genetic_interaction_gr %E>%
-        filter(cancer == !!CANCER) %N>%
-        filter(centrality_degree(mode = "all") > 0) %>%
-        mutate(node_label = ifelse(is_kras, name, NA),
-               node_label = str_remove_all(node_label, "KRAS_")) %>%
-        ggraph(layout = "stress") +
-        geom_edge_link(aes(color = genetic_interaction), width = 0.1) +
-        scale_edge_color_manual(values = comut_mutex_pal) +
-        geom_node_point(aes(color = node_label), size = 1) +
-        geom_node_text(aes(label = node_label), repel = TRUE, family = "Arial") +
-        scale_color_manual(values = short_allele_pal, na.value = NA) +
-        theme_graph() +
-        theme(
-            text = element_text(family = "Arial"),
-            plot.title = element_text(hjust = 0.5)
-        ) +
-        labs(
-            title = glue("Allele-specific genetic interactions in {CANCER}"),
-            color = "KRAS allele",
-            edge_color = "genetic\ninteraction"
-        )
-    save_path <- plot_path("20_40_highlivel-genetic-interactions",
-                           glue("genetic_interaction_network_{CANCER}.svg"))
-    ggsave_wrapper(gr_plot, save_path, size = "large")
-}
-
 
 goi_genes <- unique(genes_of_interest_df$hugo_symbol)
 
-# Same plots as above but with thicker edges.
-# Only the largest component
-set.seed(0)
-for (CANCER in unique(genetic_interaction_df$cancer)) {
-    make_thick_network_plot <- function(gr) {
-        p <- gr %>%
-            ggraph(layout = "nicely") +
-            geom_edge_link(
-                aes(color = genetic_interaction),
-                width = 0.3
-            ) +
-            scale_edge_color_manual(values = comut_mutex_pal) +
-            geom_node_point(
-                aes(color = node_color,
-                    size = node_size),
-            ) +
-            geom_node_text(
-                aes(label = node_label,
-                    size = label_size),
-                repel = TRUE,
-                family = "Arial"
-            ) +
-            scale_color_manual(values = short_allele_pal, na.value = NA) +
-            scale_size_identity() +
-            theme_graph(base_family = "Arial", base_size = 8) +
-            theme(
-                plot.title = element_blank(),
-                legend.position = "none"
-            )
-        return(p)
-    }
+# Plots the genetic interaction networks.
+make_network_plot <- function(gr) {
+    p <- gr %>%
+        ggraph(layout = "nicely") +
+        geom_edge_link(
+            aes(color = genetic_interaction),
+            width = 0.3
+        ) +
+        scale_edge_color_manual(values = comut_mutex_pal) +
+        geom_node_point(
+            aes(color = node_color,
+                size = node_size),
+        ) +
+        geom_node_text(
+            aes(label = node_label,
+                size = label_size),
+            repel = TRUE,
+            family = "Arial"
+        ) +
+        scale_color_manual(
+            values = short_allele_pal,
+            na.value = NA
+        ) +
+        scale_size_identity() +
+        theme_graph(
+            base_family = "Arial",
+            base_size = 8
+        ) +
+        theme(
+            plot.title = element_blank(),
+            legend.position = "left"
+        )
+    return(p)
+}
+
+
+for (CANCER in sort(unique(genetic_interaction_df$cancer))) {
+    set.seed(0)
     gr_plot <- genetic_interaction_gr %E>%
         filter(cancer == !!CANCER) %N>%
         filter(centrality_degree(mode = "all") > 0) %>%
@@ -120,125 +109,23 @@ for (CANCER in unique(genetic_interaction_df$cancer)) {
                node_color = node_label,
                node_size = 2,
                label_size = 5) %>%
-        make_thick_network_plot() +
+        make_network_plot() +
         labs(
-            title = glue("Genetic interactions in {CANCER}"),
-            color = "KRAS allele",
-            edge_color = "genetic\ninteraction"
+            edge_color = "interaction"
         )
-    save_path <- plot_path("20_40_highlivel-genetic-interactions",
-                           glue("genetic_interaction_network_{CANCER}_thick.svg"))
+    save_path <- plot_path(GRAPHS_DIR,
+                           glue("genetic_interaction_network_{CANCER}.svg"))
     ggsave_wrapper(gr_plot, save_path, size = "medium")
 
-    gr_plot <- genetic_interaction_gr %E>%
-        filter(cancer == !!CANCER & genetic_interaction == "comutation") %N>%
-        filter(centrality_degree(mode = "all") > 0) %>%
-        mutate(
-            node_label = ifelse(is_kras | name %in% !!goi_genes, name, NA),
-            node_label = str_remove_all(node_label, "KRAS_"),
-            node_color = case_when(
-                is_kras ~ node_label,
-                name %in% !!goi_genes ~ "Other"
-            ),
-            node_size = case_when(
-                is_kras ~ 2,
-                name %in% !!goi_genes ~ 0.5
-            ),
-            label_size = case_when(
-                is_kras ~ 5,
-                name %in% !!goi_genes ~ 2.5
-            )
-        ) %>%
-        make_thick_network_plot() +
-        labs(
-            title = glue("Genetic interactions in {CANCER}"),
-            color = "KRAS allele",
-            edge_color = "genetic\ninteraction"
-        )
-
-    save_path <- plot_path("20_40_highlivel-genetic-interactions",
-                           glue("genetic_interaction_network_{CANCER}_thick_comutation_goi.svg"))
-    ggsave_wrapper(gr_plot, save_path, size = "medium")
-
+    saveRDS(
+        gr_plot,
+        get_fig_proto_path(glue("genetic_interaction_network_{CANCER}.svg"), 2)
+    )
 }
-
-
-
-#### ---- G12V across cancers ---- ####
-
-cancer_regex_exact <- "^COAD$|^LUAD$|^PAAD$|^MM$|^SKCM$"
-
-g12v_gr <- genetic_interaction_df %>%
-    select(hugo_symbol, kras_allele, cancer, p_val, genetic_interaction) %>%
-    unique() %>%
-    filter(kras_allele == "KRAS_G12V") %>%
-    mutate(from = cancer, to = hugo_symbol) %>%
-    filter(str_replace_us(kras_allele) %in% names(allele_palette)) %>%
-    as_tbl_graph() %>%
-    filter(centrality_degree(mode = "all") > 0) %>%
-    mutate(is_cancer = str_detect(name, cancer_regex_exact),
-           node_label = ifelse(is_cancer, name, NA))
-
-g12v_plot <- g12v_gr %>%
-    ggraph(layout = "stress") +
-    geom_edge_link(aes(color = genetic_interaction), width = 0.3) +
-    scale_edge_color_manual(values = comut_mutex_pal) +
-    geom_node_point(aes(color = node_label), size = 2) +
-    geom_node_text(aes(label = node_label), repel = TRUE, family = "Arial") +
-    scale_color_manual(values = cancer_palette, na.value = NA) +
-    theme_graph() +
-    theme(
-        text = element_text(family = "Arial"),
-        plot.title = element_text(hjust = 0.5)
-    ) +
-    labs(
-        title = glue("G12V interaction network across cancers"),
-        color = "cancer",
-        edge_color = "genetic\ninteraction"
-    )
-save_path <- plot_path("20_40_highlivel-genetic-interactions",
-                       glue("genetic_interaction_network_G12V_thick.svg"))
-ggsave_wrapper(g12v_plot, save_path, width = 12, height = 8)
-
-g12v_gr_overlap <- g12v_gr %N>%
-    filter(centrality_degree(mode = "all") > 1) %N>%
-    filter(centrality_degree(mode = "all") > 0)
-
-g12v_plot_overlap <- g12v_gr_overlap %>%
-    ggraph(layout = "stress") +
-    geom_edge_link(aes(color = genetic_interaction, width = -log10(p_val + 1e-6))) +
-    scale_edge_color_manual(values = comut_mutex_pal) +
-    scale_edge_width_continuous(range = c(0.2, 2)) +
-    geom_node_point(aes(color = node_label), size = 2) +
-    geom_node_text(aes(label = name), repel = TRUE, family = "Arial") +
-    scale_color_manual(values = cancer_palette, na.value = NA) +
-    theme_graph() +
-    theme(
-        text = element_text(family = "Arial"),
-        plot.title = element_text(hjust = 0.5)
-    ) +
-    labs(
-        title = glue("G12V interaction network across cancers"),
-        color = "cancer",
-        edge_color = "genetic\ninteraction"
-    )
-save_path <- plot_path("20_40_highlivel-genetic-interactions",
-                       glue("genetic_interaction_network_G12V_overlap_thick.svg"))
-ggsave_wrapper(g12v_plot_overlap, save_path, width = 11, height = 8)
 
 
 
 #### ---- Correlate number of interactors KRAS allele frequency ---- ####
-
-# A function factory for getting integer y-axis values.
-integer_breaks <- function(n = 5, ...) {
-    fxn <- function(x) {
-        breaks <- floor(pretty(x, n, ...))
-        names(breaks) <- attr(breaks, "labels")
-        breaks
-    }
-    return(fxn)
-}
 
 interaction_counts_data <- genetic_interaction_df %>%
     mutate(allele_freq = ifelse(
@@ -271,6 +158,6 @@ interaction_counts_plot <- interaction_counts_data %>%
         color = "allele",
         shape = "interaction"
     )
-save_path <- plot_path("20_40_highlivel-genetic-interactions",
+save_path <- plot_path(GRAPHS_DIR,
                        "corr_allele-freq_num-interactors.svg")
 ggsave_wrapper(interaction_counts_plot, save_path, "medium")
