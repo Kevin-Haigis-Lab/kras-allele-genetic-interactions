5c33b93d037bfa94781e45db1186397221163ed2
Josh Cook <joshuacook0023@gmail.com>
2019-12-09 06:53:12 -0500

    copy figures to specific dir in static images of website

--- make_notebook.sh
+++ make_notebook.sh
@@ -3,23 +3,31 @@
 ## Copy the graphs from the analysis to the "reports/static/img/"
 ## to be used in the notebook.
 
-cp -r graphs reports/static/img
+REPORTS_IMG_DIR="reports/static/img"
+[ ! -d $REPORTS_IMG_DIR ] && mkdir $REPORTS_IMG_DIR
+
+cp -r graphs $REPORTS_IMG_DIR
+
+
+## Copy the subversioned figures to "reports/static/img/figures/"
+FIGURE_DIR="paper/figures"
+DEST_FIG_DIR="${REPORTS_IMG_DIR}/figures"
+[ ! -d $DEST_FIG_DIR ] && mkdir $DEST_FIG_DIR
+
+for f in $( find $FIGURE_DIR -name "Figure*svg" ); do
+	cp $f $DEST_FIG_DIR
+done
 
 
 ## Build hugo website
 
 cd reports/
-
 hugo -d ../docs
-
 cd ..
 
 
 ## Commit to repo
 
 git add -A
-
 git commit -m "build notebook: $(date)"
-
 git push
-
