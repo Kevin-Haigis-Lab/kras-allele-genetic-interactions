b53ef0b52a1a5dfd01608fc0fe58330d4ea91831
Josh Cook <joshuacook0023@gmail.com>
2019-12-12 12:25:22 -0500

    Add panels F and G as G12V oncoplots

--- paper/figures/figure_protos/make-figure_02-001.R
+++ paper/figures/figure_protos/make-figure_02-001.R
@@ -11,7 +11,7 @@ library(patchwork)
 
 #### ---- Figure theme ---- ####
 
-# General theme for Figure 2.
+#' General theme for Figure 2.
 theme_fig2 <- function() {
     theme_bw(base_size = 7, base_family = "Arial") %+replace%
     theme(
@@ -26,7 +26,8 @@ theme_fig2 <- function() {
     )
 }
 
-# Special theme for graphs from 'ggraph'.
+
+#' Special theme for graphs from 'ggraph'.
 theme_graph_fig2 <- function() {
     theme_graph(base_size = 7, base_family = "Arial") %+replace%
     theme(
@@ -42,32 +43,36 @@ theme_graph_fig2 <- function() {
     )
 }
 
-# Adjust the oncoplots.
+
+#' Adjust the oncoplots.
 adjust_oncoplot_theme <- function(pw) {
     # Top bar plot
     pw[[1]] <- pw[[1]] +
         theme(
             axis.text.y = element_text(size = 5, hjust = 1),
             plot.tag = element_text(size = 7,
-                                face = "bold",
-                                margin = margin(0, 0, 0, 0, "mm"))
+                                    face = "bold",
+                                    margin = margin(0, 0, 0, 0, "mm"))
         )
+
 
     # Middle main tile plot
     pw[[2]] <- pw[[2]] +
         scale_fill_manual(
             values = mod_variant_pal,
-            guide = guide_legend(title = NULL, ncol=1)
+            guide = guide_legend(title = NULL, ncol = 1)
         ) +
         theme(
             axis.text.y = element_text(size = 6, hjust = 1)
         )
 
-    # Top bar plot
+
+    # Right bar plot
     pw[[3]] <- pw[[3]] +
         theme(
             axis.text.x = element_text(size = 5, vjust = 1)
         )
+
 
     # Clinical feature bar (KRAS allele)
     pw[[4]] <- pw[[4]] +
@@ -75,7 +80,7 @@ adjust_oncoplot_theme <- function(pw) {
             values = short_allele_pal,
             guide = guide_legend(
                 title = NULL,
-                ncol = 2,
+                ncol = 1,
                 label.hjust = 0,
                 label.position = "right",
             )
@@ -87,7 +92,8 @@ adjust_oncoplot_theme <- function(pw) {
     return(pw)
 }
 
-# Send the legend position for the oncoplots to "none".
+
+#' Send the legend position for the oncoplots to "none".
 remove_oncoplot_legend <- function(pw) {
     # Only panels 2 and 4 have legends.
     for (i in c(2, 4)) {
@@ -95,6 +101,8 @@ remove_oncoplot_legend <- function(pw) {
     }
     return(pw)
 }
+
+
 
 
 #### ---- A. High-level comutation network for COAD ---- ####
@@ -160,13 +168,14 @@ panel_C <- read_fig_proto("enrichr_COAD", 2) +
         legend.spacing.y = unit(0, "mm"),
         legend.margin = margin(-1, 0, -1, 0, "mm"),
         legend.box.background = element_rect(fill = NA, color = NA)
-    )
+    ) +
+    labs(tag = "c")
 
 
 #### ---- D. Oncoplot ---- ####
 
 # Panel D.
-# A rainfall plot.
+# A rainfall plot of select increased comutation interactions with G12D in COAD.
 # original script: "src/20_50_rainfall-plots.R"
 
 panel_D <- read_fig_proto("COAD_G12D_comutation_oncostrip_select", FIGNUM)
@@ -179,16 +188,50 @@ panel_D_leg <- (panel_D_leg_1 / panel_D_leg_2)
 
 panel_D <- remove_oncoplot_legend(panel_D)
 
+
 #### ---- E. Oncoplot ---- ####
 
 # Panel E.
-# A rainfall plot.
+# A rainfall plot of select reduced comutation interactions with G12D in COAD.
 # original script: "src/20_50_rainfall-plots.R"
 
 panel_E <- read_fig_proto("COAD_G12D_exclusivity_oncostrip_select", FIGNUM)
 panel_E <- adjust_oncoplot_theme(panel_E)
 panel_E[[1]] <- panel_E[[1]] + labs(tag = "e")
 panel_E <- remove_oncoplot_legend(panel_E)
+
+
+
+
+
+#### ---- F. Oncoplot ---- ####
+
+# Panel F.
+# A rainfall plot of select increased comutation interactions with G12D in COAD.
+# original script: "src/20_50_rainfall-plots.R"
+
+panel_F <- read_fig_proto("COAD_G12V_comutation_oncostrip_select", FIGNUM)
+panel_F <- adjust_oncoplot_theme(panel_F)
+panel_F[[1]] <- panel_F[[1]] + labs(tag = "f")
+panel_F <- remove_oncoplot_legend(panel_F)
+
+
+#### ---- G. Oncoplot ---- ####
+
+# Panel E.
+# A rainfall plot of select reduced comutation interactions with G12D in COAD.
+# original script: "src/20_50_rainfall-plots.R"
+
+panel_G <- read_fig_proto("COAD_G12V_exclusivity_oncostrip_select", FIGNUM)
+panel_G <- adjust_oncoplot_theme(panel_G)
+panel_G[[1]] <- panel_G[[1]] + labs(tag = "g")
+panel_G <- remove_oncoplot_legend(panel_G)
+
+
+
+
+
+
 
 
 
@@ -200,17 +243,25 @@ panel_E <- remove_oncoplot_legend(panel_E)
     row_1 <- (panel_A | panel_B | panel_C) +
         plot_layout(widths = c(2, 2, 1))
 
-    row_2 <- plot_spacer() + panel_D + panel_E + panel_D_leg +
-        plot_layout(
-            nrow = 1,
-            widths = c(1, 1000, 1000, 400)
-        )
+    # row_2 <- plot_spacer() + panel_D + panel_E + panel_D_leg +
+    #     plot_layout(
+    #         nrow = 1,
+    #         widths = c(1, 1000, 1000, 400)
+    #     )
 
-    row_3 <- plot_spacer()
+
+
+
+    rows_2_3 <- plot_spacer() +
+    (
+        (panel_D | panel_E) / (panel_F | panel_G )
+    ) +
+    panel_D_leg +
+    plot_layout(widths = c(1, 2000, 300))
 
     # COMPLETE FIGURE
-    full_figure <- (row_1) / (row_2)  / (row_3) +
-        plot_layout(heights = c(2, 1, 2)) +
+    full_figure <- (row_1) / (rows_2_3) +
+        plot_layout(heights = c(2, 2, 1)) +
         plot_annotation(
             title = glue("Figure {FIGNUM}"),
             theme = theme(
