985c46ddbc91ce6e6c84d0937d9522275d4016cb
Josh Cook <joshuacook0023@gmail.com>
2019-12-13 09:49:41 -0500

    first draft of S Fig 1

--- paper/figures/figure_protos/make-suppfigure_01-001.R
+++ paper/figures/figure_protos/make-suppfigure_01-001.R
@@ -1,10 +1,29 @@
 
 # Supplemental Figure 1. Barplots of frequency of all alleles
 
-FIGNUM <- "S2"
+FIGNUM <- 1
 VERSION <- 1
 FIGFILENAME <- glue("figure_{FIGNUM}_{VERSION}.svg")
 FIG_DIMENSIONS <- get_figure_dimensions(2, "short")
+
+
+FIG_DIMENSIONS$height <- 100  # specific change for this figure
+
+#### ---- Figure theme ---- ####
+
+theme_figS1 <- function() {
+    theme_bw(base_size = 7, base_family = "Arial") %+replace%
+    theme(
+        plot.title = element_text(size = 7, hjust = 0.5),
+        axis.title = element_text(size = 6),
+        axis.text.y = element_text(size = 5, hjust = 1),
+        axis.text.x = element_blank(),
+        axis.ticks = element_blank(),
+        plot.tag = element_text(size = 7,
+                                face = "bold",
+                                margin = margin(-3, -3, -3, -3, "mm"))
+    )
+}
 
 
 
@@ -12,3 +31,43 @@ FIG_DIMENSIONS <- get_figure_dimensions(2, "short")
 
 # The distribution of the alleles (no "Other" group) per cancer.
 # original script: "src/90_05_kras-allele-distribution.R"
+
+panel_A <- read_fig_proto("allele_dist_barplot_stackplot_all",
+                         FIGNUM,
+                         supp = TRUE) %>%
+    wrap_plots() &
+    theme_figS1() %+replace%
+    theme(
+        axis.title.y = element_blank(),
+    )
+
+for (i in 1:4) {
+    panel_A[[i]][[2]] <- panel_A[[i]][[2]] *
+        theme_fig1() %+replace%
+        theme(
+            axis.title = element_blank(),
+            axis.text.x = element_blank()
+        )
+}
+
+#### ---- Figure assembly ---- ####
+
+{
+    # COMPLETE FIGURE
+    full_figure <- panel_A +
+        plot_annotation(
+            title = glue("Figure {FIGNUM}"),
+            theme = theme(
+                plot.title = element_text(size = 10,
+                                          family = "Arial",
+                                          hjust = 0)
+            )
+        )
+
+    save_figure(
+        full_figure,
+        figure_num = FIGNUM,
+        supp = TRUE,
+        dim = FIG_DIMENSIONS
+    )
+}
