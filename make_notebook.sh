672825d82ea14990a990e0c36825142dda277963
Josh Cook <joshuacook0023@gmail.com>
2019-12-14 20:19:06 -0500

    build notebook: Sat Dec 14 20:19:06 EST 2019

--- make_notebook.sh
+++ make_notebook.sh
@@ -9,13 +9,20 @@ REPORTS_IMG_DIR="reports/static/img"
 cp -r graphs $REPORTS_IMG_DIR
 
 
-## Copy the subversioned figures to "reports/static/img/figures/"
+## Copy the subversioned figures to "reports/static/img/figures/" 
 FIGURE_DIR="paper/figures"
 DEST_FIG_DIR="${REPORTS_IMG_DIR}/figures"
 [ ! -d $DEST_FIG_DIR ] && mkdir $DEST_FIG_DIR
 
-for f in $( find $FIGURE_DIR -name "Figure*svg" ); do
+for f in $( find $FIGURE_DIR -name "*Figure*svg" ); do
 	cp $f $DEST_FIG_DIR
+done
+
+
+## Copy the full figures to "reports/content/home/gallery/gallery/"
+GALLERY_DIR="reports/content/home/gallery/gallery"
+for f in $( find $FIGURE_DIR -d 1 -name "*Figure*svg" ); do
+	cp $f GALLERY_DIR
 done
 
 
