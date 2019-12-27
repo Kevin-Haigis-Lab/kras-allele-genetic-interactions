2b9d1e0f98a1ff5bf5a951669b966e6b68caa563
Josh Cook <joshuacook0023@gmail.com>
2019-12-26 21:07:14 -0800

    doc simple fxns; add column of standardized terms

--- munge/60_enrichr-gene-sets.R
+++ munge/60_enrichr-gene-sets.R
@@ -1,16 +1,18 @@
 # Prepare the gene sets used by Enrichr downloaded from their library:
 #   https://amp.pharm.mssm.edu/Enrichr/#stats
 
-
+# Extract the term from the line of the gene set file.
 get_terms <- function(l) {
     unlist(str_split(l, "\t"))[[1]]
 }
 
+# Get the genes for a gene set from the line of the gene set file.
 get_genes <- function(l) {
     a <- unlist(str_split(l, "\t"))[-1]
     a[a != ""]
 }
 
+# Parse a gene set file from Enrichr's library.
 parse_geneset_file <- function(path) {
     lines <- readLines(path)
     terms <- purrr::map_chr(lines, get_terms)
@@ -33,6 +35,8 @@ ProjectTemplate::cache("enrichr_genesets",
         ) %>%
         mutate(data = map(path, parse_geneset_file)) %>%
         unnest(data) %>%
+        mutate(std_term = map2_chr(term, datasource, mod_term_for_datasource),
+               std_term = standardize_enricher_terms(std_term)) %>%
         select(-path)
 
     return(enrichr_genesets)
