8f48783e777abf188501a03d995ab82b7f71ae3e
Josh Cook <joshuacook0023@gmail.com>
2020-02-11 15:29:28 -0500

    include edge coloration based on the node group

--- src/40_15_comparing-COAD-allele-subnetworks.R
+++ src/40_15_comparing-COAD-allele-subnetworks.R
@@ -30,6 +30,12 @@ make_gray_pal <- function(max_ct, start = 0.8, end = 0.5) {
     return(pal)
 }
 
+make_brown_pal <- function(max_ct) {
+    pal <- colorRampPalette(c("burlywood2", "burlywood4"))(max_ct)
+    names(pal) <- paste(seq(2, max_ct), "alleles", sep = " ")
+    return(pal)
+}
+
 
 factor_multiple_node_colors <- function(nc) {
     alleles <- unique(nc[!str_detect(nc, "allele")])
@@ -75,8 +81,58 @@ plot_overlap_comparison_graph <- function(gr, special_labels = NULL) {
     return(p)
 }
 
+
+annotate_edges_with_clustering <- function(gr) {
+    mod_gr <- gr %N>%
+        mutate(group = group_label_prop()) %E>%
+        mutate(edge_color = ifelse(
+            .N()$node_color[to] == .N()$node_color[from],
+            as.character(.N()$node_color[to]), NA
+        ))
+    return(mod_gr)
+}
+
+
+plot_overlap_comparison_graph2 <- function(gr, special_labels = NULL) {
+    max_ct <- max(str_count(igraph::V(gr)$allele, ",")) + 1
+    pal <- c(short_allele_pal,
+             make_brown_pal(max_ct),
+             "special" = "indianred1")
+    p <- gr %>%
+        mutate(
+            node_color = case_when(
+                name %in% !!special_labels ~ "special",
+                str_detect(allele, ",") ~ get_allele_count_label(allele),
+                TRUE ~ allele
+            ),
+            node_color = factor_multiple_node_colors(node_color)
+        ) %>%
+        annotate_edges_with_clustering() %N>%
+        ggraph(layout = "kk") +
+        geom_edge_link(
+            aes(color = edge_color),
+            width = 1, alpha = 0.5
+        ) +
+        geom_node_point(
+            aes(color = node_color),
+            size = 3
+        ) +
+        geom_node_text(
+            aes(label = name),
+            size = 2, family = "Arial", repel = TRUE
+        ) +
+        scale_color_manual(values = pal) +
+        scale_edge_color_manual(
+            values = pal,
+            na.value = "grey70",
+            guide = FALSE
+        ) +
+        theme_graph()
+    return(p)
+}
+
 print_node_names <- function(gr) {
-    cat(igraph::V(gr)$name, sep = "\n")
+    # cat(igraph::V(gr)$name, sep = "\n")
     return(gr)
 }
 
@@ -108,8 +164,8 @@ coad_overlap_comparison_plot <- tibble(cancer = c("COAD", "COAD", "COAD"),
     ) %>%
     make_overlap_comparison_graph() %>%
     print_node_names() %>%
-    plot_overlap_comparison_graph(special_labels = special_nodes)
-
+    plot_overlap_comparison_graph2(special_labels = special_nodes)
+    # plot_overlap_comparison_graph(special_labels = special_nodes)
 
 ggsave_wrapper(
     coad_overlap_comparison_plot,
