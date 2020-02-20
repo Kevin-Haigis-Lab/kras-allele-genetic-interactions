6092e382c73ff004ee42403c4267909e867166fd
Josh Cook <joshuacook0023@gmail.com>
2020-02-20 17:07:10 -0500

    function to save pheatmap to PDF

--- lib/ggplot2_helpers.R
+++ lib/ggplot2_helpers.R
@@ -89,6 +89,14 @@ save_pheatmap_svg <- function(x, save_path, size = NA, width = NA, height = NA) 
     dev.off()
 }
 
+save_pheatmap_pdf <- function(x, save_path, size = NA, width = NA, height = NA) {
+    size <- decide_size(size = size[[1]], width = width, height = height)
+    pdf(save_path, width = size[[1]], height = size[[2]])
+    grid::grid.newpage()
+    grid::grid.draw(x$gtable)
+    dev.off()
+}
+
 
 #### ---- Adjust colors ---- ####
 
