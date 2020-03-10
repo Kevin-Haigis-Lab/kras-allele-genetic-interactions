b6f73517252c2e81e506f3c9409e7427795b19c8
Josh Cook <joshuacook0023@gmail.com>
2020-03-09 23:01:59 -0400

    feat: script to convert SVG figures to PDFs.

---
+++ paper/figures_to_pdf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -p priority
+#SBATCH -c 1
+#SBATCH -t 5
+#SBATCH --mem 1G
+#SBATCH -o /dev/null
+#SBATCH -e /dev/null
+
+# A simple script for converting the SVG figures into PDF files.
+
+module load gcc python/3.7.4
+
+source ~/base-env/bin/activate
+
+ORIGINAL_DIR=$(pwd)
+
+cd paper/figures
+
+for img in *svg
+do
+	echo "Converting $img"
+	PDF_NAME="pdfs/$(basename $img svg)pdf"
+	cairosvg $img -o $PDF_NAME
+done
+
+deactivate
+
+cd $ORIGINAL_DIR
+
+exit
