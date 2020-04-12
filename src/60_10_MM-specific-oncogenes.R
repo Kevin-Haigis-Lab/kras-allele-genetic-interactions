9c97a0814c0038dc95a4c9ad7c836c8fd2d4268a
Josh Cook <joshuacook0023@gmail.com>
2020-04-12 17:41:29 -0400

    style: include a truncated version of the same heatmap for figures
    I refactored so that the plot-making portions were functions. This does not change the previous output. I then added a section to the end to make the exact same figure, except only for the top half of the genes. These are labeled 'TRUNCATED'.

--- src/60_10_MM-specific-oncogenes.R
+++ src/60_10_MM-specific-oncogenes.R
@@ -63,46 +63,53 @@ mut_freq_order <- mm_mut_df %>%
     arrange(n) %>%
     pull(hugo_symbol)
 
+
+make_mm_comut_heatmap <- function(df) {
+    df %>%
+        mutate(
+            allele = factor_alleles(allele),
+            hugo_symbol = factor(hugo_symbol, levels = mut_freq_order),
+            label = round(freq_allele_comuts, 3) * 100,
+            label = case_when(
+                !is.na(genetic_interaction) ~ as.character(label),
+                label > 0 ~ as.character(label),
+                label == 0 ~ "-"),
+            label_face = ifelse(!is.na(genetic_interaction), "bold", "plain")
+        ) %>%
+        ggplot(aes(x = allele, y = hugo_symbol)) +
+        geom_tile(
+            aes(fill = freq_allele_comuts),
+            color = "grey20"
+        ) +
+        geom_text(
+            aes(label = label,
+                fontface = label_face),
+            family = "Arial",
+            size = 2
+        ) +
+        scale_fill_gradient2(
+            low = "dodgerblue",
+            high = "tomato",
+            mid = "grey90",
+            midpoint = 0.10
+        ) +
+        scale_x_discrete(expand = c(0, 0)) +
+        scale_y_discrete(expand = c(0, 0)) +
+        theme_bw(base_size = 7, base_family = "Arial") +
+        theme(
+            axis.title = element_blank(),
+            axis.ticks = element_blank(),
+            legend.position = "bottom"
+        ) +
+        labs(
+            fill = "comutation frequency"
+        )
+}
+
 mm_comut_heatmap <- mm_comut_df %>%
     filter(num_allele_samples > !!MIN_NUM_ALLELE_SAMPLES) %>%
-    mutate(
-        allele = factor_alleles(allele),
-        hugo_symbol = factor(hugo_symbol, levels = mut_freq_order),
-        label = round(freq_allele_comuts, 3) * 100,
-        label = case_when(
-            !is.na(genetic_interaction) ~ as.character(label),
-            label > 0 ~ as.character(label),
-            label == 0 ~ "-"),
-        label_face = ifelse(!is.na(genetic_interaction), "bold", "plain")
-    ) %>%
-    ggplot(aes(x = allele, y = hugo_symbol)) +
-    geom_tile(
-        aes(fill = freq_allele_comuts),
-        color = "grey20"
-    ) +
-    geom_text(
-        aes(label = label,
-            fontface = label_face),
-        family = "Arial",
-        size = 2
-    ) +
-    scale_fill_gradient2(
-        low = "dodgerblue",
-        high = "tomato",
-        mid = "grey90",
-        midpoint = 0.10
-    ) +
-    scale_x_discrete(expand = c(0, 0)) +
-    scale_y_discrete(expand = c(0, 0)) +
-    theme_bw(base_size = 7, base_family = "Arial") +
-    theme(
-        axis.title = element_blank(),
-        axis.ticks = element_blank(),
-        legend.position = "bottom"
-    ) +
-    labs(
-        fill = "comutation frequency"
-    )
+    make_mm_comut_heatmap()
+
 ggsave_wrapper(
     mm_comut_heatmap,
     plot_path(GRAPHS_DIR, "mm_comut_heatmap.svg"),
@@ -110,47 +117,59 @@ ggsave_wrapper(
 )
 
 
+make_allele_freq_barplot <- function(df) {
+    df %>%
+        mutate(allele = factor_alleles(allele)) %>%
+        ggplot(aes(x = allele, y = num_allele_samples)) +
+        geom_col(aes(fill = log10(num_allele_samples))) +
+        scale_fill_viridis_c(guide = FALSE) +
+        scale_y_continuous(
+            breaks = c(5, 10, 50, 200, 500),
+            expand = expansion(mult = c(0, 0.05)),
+            trans = my_trans_log10
+        ) +
+        scale_x_discrete(expand = c(0, 0)) +
+        theme_bw(base_family = "Arial", base_size = 7) +
+        theme(
+            axis.title.x = element_blank(),
+            axis.text.x = element_blank(),
+            axis.ticks = element_blank()
+        ) +
+        labs(y = "num. tumor\nsamples")
+}
+
 allele_freq_barplot <- cancer_allele_count_df %>%
     filter(num_allele_samples > !!MIN_NUM_ALLELE_SAMPLES) %>%
-    mutate(allele = factor_alleles(allele)) %>%
-    ggplot(aes(x = allele, y = num_allele_samples)) +
-    geom_col(aes(fill = log10(num_allele_samples))) +
-    scale_fill_viridis_c(guide = FALSE) +
-    scale_y_continuous(
-        breaks = c(5, 10, 50, 200, 500),
-        expand = expansion(mult = c(0, 0.05)),
-        trans = my_trans_log10
-    ) +
-    scale_x_discrete(expand = c(0, 0)) +
-    theme_bw(base_family = "Arial", base_size = 7) +
-    theme(
-        axis.title.x = element_blank(),
-        axis.text.x = element_blank(),
-        axis.ticks = element_blank()
-    ) +
-    labs(y = "num. tumor\nsamples")
+    make_allele_freq_barplot()
+
+
+make_gene_freq_barplot <- function(df) {
+    df %>%
+        mutate(hugo_symbol = fct_reorder(hugo_symbol, n)) %>%
+        ggplot(aes(x = hugo_symbol, y = n)) +
+        geom_col(aes(fill = log10(n))) +
+        scale_fill_viridis_c(guide = FALSE) +
+        coord_flip() +
+        scale_y_continuous(
+            expand = expansion(mult = c(0, 0.05))
+        ) +
+        scale_x_discrete(expand = c(0, 0)) +
+        theme_bw(base_family = "Arial", base_size = 7) +
+        theme(
+            axis.title.y = element_blank(),
+            axis.text.y = element_blank(),
+            axis.ticks = element_blank(),
+            axis.text.x = element_text(angle = 270, hjust = 0)
+        ) +
+        labs(y = "num. mutant\nsamples")
+}
+
 
 gene_freq_barplot <- mm_mut_df %>%
     group_by(hugo_symbol) %>%
     summarise(n = n_distinct(tumor_sample_barcode)) %>%
     ungroup() %>%
-    mutate(hugo_symbol = fct_reorder(hugo_symbol, n)) %>%
-    ggplot(aes(x = hugo_symbol, y = n)) +
-    geom_col(aes(fill = log10(n))) +
-    scale_fill_viridis_c(guide = FALSE) +
-    coord_flip() +
-    scale_y_continuous(
-        expand = expansion(mult = c(0, 0.05))
-    ) +
-    scale_x_discrete(expand = c(0, 0)) +
-    theme_bw(base_family = "Arial", base_size = 7) +
-    theme(
-        axis.title.y = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks = element_blank(),
-        axis.text.x = element_text(angle = 270, hjust = 0)
-    ) +
-    labs(y = "num. mutant\nsamples")
+    make_gene_freq_barplot()
 
 patch <-
     (allele_freq_barplot + plot_spacer() + plot_layout(widths = c(10, 2))) /
@@ -164,3 +183,43 @@ ggsave_wrapper(patch,
 saveFigRds(mm_comut_heatmap, "mm_comut_heatmap")
 saveFigRds(allele_freq_barplot, "allele_freq_barplot")
 saveFigRds(gene_freq_barplot, "gene_freq_barplot")
+
+
+
+#### ---- A truncated version for figure ---- ####
+
+
+truncated_mm_oncogenes <- tail(mut_freq_order,
+                               n = which(rev(mut_freq_order) == "TP53"))
+
+mm_comut_heatmap <- mm_comut_df %>%
+    filter(num_allele_samples > !!MIN_NUM_ALLELE_SAMPLES) %>%
+    filter(hugo_symbol %in% !!truncated_mm_oncogenes) %>%
+    make_mm_comut_heatmap()
+
+allele_freq_barplot <- cancer_allele_count_df %>%
+    filter(num_allele_samples > !!MIN_NUM_ALLELE_SAMPLES) %>%
+    make_allele_freq_barplot()
+
+gene_freq_barplot <- mm_mut_df %>%
+    filter(hugo_symbol %in% !!truncated_mm_oncogenes) %>%
+    group_by(hugo_symbol) %>%
+    summarise(n = n_distinct(tumor_sample_barcode)) %>%
+    ungroup() %>%
+    make_gene_freq_barplot()
+
+patch <-
+    (allele_freq_barplot + plot_spacer() + plot_layout(widths = c(10, 2))) /
+    (mm_comut_heatmap + gene_freq_barplot + plot_layout(widths = c(10, 2))) +
+    plot_layout(heights = c(2, 10))
+ggsave_wrapper(
+    patch,
+    plot_path(GRAPHS_DIR, "margin_barplots_heatmap_patchwork_TRUNCATED.svg"),
+    "medium"
+)
+
+
+saveFigRds(mm_comut_heatmap, "mm_comut_heatmap_TRUNCATED")
+saveFigRds(allele_freq_barplot, "allele_freq_barplot_TRUNCATED")
+saveFigRds(gene_freq_barplot, "gene_freq_barplot_TRUNCATED")
+
