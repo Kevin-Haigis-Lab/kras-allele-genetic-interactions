c5819db573ed75e0eae00ab52be47f84b2750128
Josh Cook <joshuacook0023@gmail.com>
2020-05-07 19:52:13 -0400

    analysis: no more lung in the DepMap analysis

--- src/10_56_luad_depmap_G12C-cisplatin-sensitivity.R
+++ src/10_56_luad_depmap_G12C-cisplatin-sensitivity.R
@@ -8,33 +8,33 @@ GRAPHS_DIR <- "10_56_luad_depmap_G12C-cisplatin-sensitivity"
 reset_graph_directory(GRAPHS_DIR)
 
 
-model1_tib %>%
-    filter(cancer == "LUAD" & hugo_symbol == "POLB") %>%
-    pull(allele_aov) %>%
-    purrr::map(~ broom::glance(.x)) %>%
-    knitr::kable()
+# depmap_model_workflow_res %>%
+#     filter(cancer == "LUAD" & hugo_symbol == "POLB") %>%
+#     pull(allele_aov) %>%
+#     purrr::map(~ broom::glance(.x)) %>%
+#     knitr::kable()
 
-polb_boxplot <- model1_tib %>%
-    filter(cancer == "LUAD" & hugo_symbol == "POLB") %>%
-    select(cancer, hugo_symbol, data) %>%
-    unnest(data) %>%
-    ggplot(aes(x = allele, y = gene_effect)) +
-    geom_boxplot(aes(color = allele), outlier.shape = NA) +
-    geom_jitter(aes(color = allele), width = 0.2) +
-    scale_color_manual(values = short_allele_pal) +
-    theme_bw(base_size = 7, base_family = "arial") +
-    theme(
-        axis.title.x = element_blank()
-    ) +
-    labs(
-        y = "dependency score"
-    )
-ggsave_wrapper(
-    polb_boxplot,
-    plot_path(GRAPHS_DIR, "polb_boxplot.svg"),
-    "small"
-)
+# polb_boxplot <- model1_tib %>%
+#     filter(cancer == "LUAD" & hugo_symbol == "POLB") %>%
+#     select(cancer, hugo_symbol, data) %>%
+#     unnest(data) %>%
+#     ggplot(aes(x = allele, y = gene_effect)) +
+#     geom_boxplot(aes(color = allele), outlier.shape = NA) +
+#     geom_jitter(aes(color = allele), width = 0.2) +
+#     scale_color_manual(values = short_allele_pal) +
+#     theme_bw(base_size = 7, base_family = "arial") +
+#     theme(
+#         axis.title.x = element_blank()
+#     ) +
+#     labs(
+#         y = "dependency score"
+#     )
+# ggsave_wrapper(
+#     polb_boxplot,
+#     plot_path(GRAPHS_DIR, "polb_boxplot.svg"),
+#     "small"
+# )
 
 
-genetic_interaction_df %>%
-    filter(cancer == "LUAD" & hugo_symbol == "POLB")
+# genetic_interaction_df %>%
+#     filter(cancer == "LUAD" & hugo_symbol == "POLB")
