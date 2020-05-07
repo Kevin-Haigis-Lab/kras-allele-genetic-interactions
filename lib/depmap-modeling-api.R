ad5f78a6e14785634f18288bb5cbf78f2a69e387
Josh Cook <joshuacook0023@gmail.com>
2020-05-07 18:18:54 -0400

    feat: API for filtering and unnesting of the df from the DepMap analysis workflow

---
+++ lib/depmap-modeling-api.R
@@ -0,0 +1,20 @@
+
+
+#### ---- Filtering the workflow ---- ####
+
+# A standard way to unnest the RNA-expr. linear model results.
+unnest_rna_lm_depmap_model_workflow <- function(df) {
+    df %>%
+        unnest(rna_lm_stats,  names_sep = "_")
+}
+
+# A single function that will filter the results of the above workflow.
+# This will maintain consistency throughout the project.
+filter_depmap_model_workflow_res <- function(df) {
+    df %>%
+        unnest_rna_lm_depmap_model_workflow() %>%
+        filter(rna_lm_stats_p_value >= 0.05) %>%
+        filter(ismut_pvalue >= 0.05) %>%
+        filter(allele_aov_pvalue < 0.01) %>%
+        filter(ova_any_sig)
+}
