915f9985a60fb6de53a535250f97975101af7741
Josh Cook <joshuacook0023@gmail.com>
2020-08-31 10:15:34 -0400

    style: use `message()` instead of `print()`

--- src/20_05_make-bipartite-graph.R
+++ src/20_05_make-bipartite-graph.R
@@ -1,9 +1,9 @@
 
-print("Begninning script.")
+message("Begninning script.")
 
 .libPaths(c(.libPaths(), "/home/jc604/R-3.5.1/library"))
 
-print("Loading libraries.")
+message("Loading libraries.")
 
 library(tidygraph)
 library(wext)
@@ -15,22 +15,22 @@ library(wext)
 # WRAPPER: make the sample-gene bipartite graph
 make_bipartite_graph <- function(data_file, save_file) {
 
-    print("Beginning `make_bipartite_graph()`.")
-    print(glue::glue("'data_file': {data_file}"))
-    print(glue::glue("'save_file': {save_file}"))
+    message("Beginning `make_bipartite_graph()`.")
+    message(glue::glue("'data_file': {data_file}"))
+    message(glue::glue("'save_file': {save_file}"))
 
-    print("Reading in data.")
+    message("Reading in data.")
 
     dat <- data.table::fread(data_file)
 
-    print("Making bipartite graph.")
+    message("Making bipartite graph.")
 
     bgr <- wext::make_sample_gene_bipartite(
         s = unlist(dat[, 1]),
         g = unlist(dat[, 2])
     )
 
-    print("Saving graph.")
+    message("Saving graph.")
 
     saveRDS(bgr, save_file)
 }
@@ -43,4 +43,4 @@ make_bipartite_graph(
     save_file = snakemake@output[["save_name"]]
 )
 
-print("Finished script.")
+message("Finished script.")
