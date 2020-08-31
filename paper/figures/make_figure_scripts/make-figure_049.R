09346dfbad675df7628debd079584d2311d3a68e
Josh Cook <joshuacook0023@gmail.com>
2020-08-31 15:26:06 -0400

    analysis: supp fig for mut sig associations with KRAS alleles
    - remove hypermutants (just as in Fig 1)
    - modify the heatmaps
    - add boxplots of two signatures per cancer
    - make Fig 49 (will be supplemental)

---
+++ paper/figures/make_figure_scripts/make-figure_049.R
@@ -0,0 +1,114 @@
+# Figure 049. #> Associations between mutational signature level and KRAS
+#   alleles.
+
+FIGNUM <- 49
+
+#> SET THE FIGURE DIMENSIONS
+FIG_DIMENSIONS <- get_figure_dimensions(2, "tall")
+
+
+theme_fig49 <- function(tag_margin = margin(-1, -1, -1, -1, "mm")) {
+    theme_comutation() %+replace%
+    theme(
+        legend.title = element_blank(),
+        plot.tag = element_text(size = 7,
+                                face = "bold",
+                                margin = tag_margin)
+    )
+}
+
+theme_msig_heatmap <- function() {
+    theme_fig49() %+replace%
+        theme(axis.title = element_blank(),
+              legend.title = element_text(),
+              strip.background = element_blank(),
+              strip.text = element_text(size = 7, face = "bold"),
+              panel.grid = element_blank(),
+              axis.ticks = element_blank(),
+              axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1,
+                                         size = 6))
+}
+
+
+theme_msig_boxplot <- function() {
+    theme_fig49() %+replace%
+        theme(
+            axis.title.x = element_blank(),
+            axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1,
+                                       size = 6),
+            axis.ticks = element_blank(),
+            strip.background = element_blank(),
+            strip.text = element_text(size = 7, face = "bold")
+        )
+}
+
+
+#### ---- A. COAD ---- ####
+# Associations between KRAS alleles and mutational sigs. in COAD
+# original script: "src/50_35_mutational-signature-allele-associations.R"
+
+panel_A1 <- read_fig_proto("COAD_allele-sig-associate") +
+    theme_msig_heatmap() +
+    labs(tag = "a")
+
+panel_A2 <- read_fig_proto("COAD_allele-sig-boxplots") +
+    theme_msig_boxplot()
+
+
+
+#### ---- B. LUAD ---- ####
+# Associations between KRAS alleles and mutational sigs. in LUAD
+# original script: "src/50_35_mutational-signature-allele-associations.R"
+
+panel_B1 <- read_fig_proto("LUAD_allele-sig-associate") +
+    theme_msig_heatmap() +
+    labs(tag = "b")
+
+panel_B2 <- read_fig_proto("LUAD_allele-sig-boxplots") +
+    theme_msig_boxplot()
+
+
+
+#### ---- C. MM ---- ####
+# Associations between KRAS alleles and mutational sigs. in MM
+# original script: "src/50_35_mutational-signature-allele-associations.R"
+
+panel_C1 <- read_fig_proto("MM_allele-sig-associate") +
+    theme_msig_heatmap() +
+    theme(legend.box = "horizontal") +
+    labs(tag = "c")
+
+panel_C2 <- read_fig_proto("MM_allele-sig-boxplots") +
+    theme_msig_boxplot()
+
+
+#### ---- D. PAAD ---- ####
+# Associations between KRAS alleles and mutational sigs. in PAAD
+# original script: "src/50_35_mutational-signature-allele-associations.R"
+
+panel_D1 <- read_fig_proto("PAAD_allele-sig-associate") +
+    theme_msig_heatmap() +
+    labs(tag = "d")
+
+panel_D2 <- read_fig_proto("PAAD_allele-sig-boxplots") +
+    theme_msig_boxplot()
+
+
+#### ---- Figure assembly ---- ####
+
+{
+    # COMPLETE FIGURE
+    full_figure <- (
+        ((panel_A1 + panel_A2) + plot_layout(widths = c(2, 3))) /
+        ((panel_B1 + panel_B2) + plot_layout(widths = c(2, 3))) /
+        ((panel_C1 + panel_C2) + plot_layout(widths = c(2, 3))) /
+        ((panel_D1 + panel_D2) + plot_layout(widths = c(2, 3)))
+    ) +
+        plot_layout(heights = c(2, 2, 1, 2))
+
+    save_figure(
+        full_figure,
+        figure_num = FIGNUM,
+        dim = FIG_DIMENSIONS
+    )
+}
--- src/50_35_mutational-signature-allele-associations.R
+++ src/50_35_mutational-signature-allele-associations.R
@@ -5,6 +5,7 @@ GRAPHS_DIR <- "50_35_mutational-signature-allele-associations"
 reset_graph_directory(GRAPHS_DIR)
 
 library(ggasym)
+library(ggbeeswarm)
 
 calc_cohens_d <- function(g1, g2, data) {
     allele_values <- function(a, d) d$contribution[d$ras_allele == a]
@@ -44,6 +45,7 @@ pairwise_ttest_msigs <- function(data, min_num_samples = 10) {
 
 
 allele_signature_associations <- mutsig_noartifact_df %>%
+    filter(!is_hypermutant) %>%
     group_by(cancer, signature) %>%
     nest() %>%
     mutate(t_test_res = map(data, pairwise_ttest_msigs)) %>%
@@ -61,6 +63,17 @@ assign_stars <- function(pval) {
 }
 
 
+modify_mutsig_names <- function(sig) {
+    case_when(
+        sig == "N3V2" ~ "N",
+        TRUE ~ sig
+    )
+}
+
+factor_signatures <- function(sig) {
+    factor(sig, levels = c(names(mutsig_pal), "N"))
+}
+
 plot_allele_sig_association_grids <- function(cancer, data) {
     mod_data <- data %>%
         group_by(signature) %>%
@@ -68,11 +81,13 @@ plot_allele_sig_association_grids <- function(cancer, data) {
         mutate(data = map(data, ~ asymmetrise(.x, group1, group2))) %>%
         unnest(data) %>%
         ungroup() %>%
-        mutate(signature = factor(signature, levels = names(mutsig_pal)),
+        mutate(signature = modify_mutsig_names(signature)) %>%
+        mutate(signature = factor_signatures(signature),
                group1 = str_remove(group1, "KRAS_"),
                group2 = str_remove(group2, "KRAS_"),
                group1 = factor_alleles(group1, reverse = FALSE),
                group2 = factor_alleles(group2, reverse = FALSE))
+
 
     p <- mod_data %>%
         mutate(p_value = ifelse(is.na(p_value), 1, p_value),
@@ -86,11 +101,15 @@ plot_allele_sig_association_grids <- function(cancer, data) {
         scale_fill_tl_distiller(type = "seq",
                                 palette = "OrRd",
                                 na.value = "grey70",
-                                guide = guide_colorbar(order = 1)) +
+                                guide = guide_colorbar(barwidth = unit(3, "mm"),
+                                                       barheight = unit(18, "mm"),
+                                                       order = 1)) +
         scale_fill_br_distiller(type = "div",
                                 palette = "PiYG",
                                 na.value = "grey70",
-                                guide = guide_colorbar(order = 2)) +
+                                guide = guide_colorbar(barwidth = unit(3, "mm"),
+                                                       barheight = unit(18, "mm"),
+                                                       order = 2)) +
         scale_x_discrete(expand = c(0, 0)) +
         scale_y_discrete(expand = c(0, 0)) +
         theme_bw(base_size = 7, base_family = "Arial") +
@@ -103,11 +122,14 @@ plot_allele_sig_association_grids <- function(cancer, data) {
         labs(fill_br = "Cohen's d",
              fill_tl = "adj. p-value",
              title = cancer)
+
+    plt_name <- as.character(glue("{cancer}_allele-sig-associate.svg"))
     ggsave_wrapper(
         p,
-        plot_path(GRAPHS_DIR, glue("{cancer}_allele-sig-associate.svg")),
+        plot_path(GRAPHS_DIR, plt_name),
         "large"
     )
+    saveFigRds(p, plt_name)
     return(NULL)
 }
 
@@ -119,3 +141,66 @@ allele_signature_associations %>%
     group_by(cancer) %>%
     nest() %>%
     mutate(a = walk2(cancer, data, plot_allele_sig_association_grids))
+
+
+#### ---- Box-plots of specific signatures ---- ####
+
+cancer_sigs_for_boxplot <- tribble(
+    ~ cancer, ~ signature,
+    "COAD", c("8", "18"),
+    "LUAD", c("4", "18"),
+    "MM", c("2", "9"),
+    "PAAD", c("8", "17")
+) %>%
+    unnest(signature)
+
+
+plot_signature_boxplots <- function(cancer, data, min_num_samples = 10) {
+    set.seed(0)
+    alleles_to_use <- get_alleles_to_test(data, min_num_samples)
+    plt_data <- data %>%
+        filter(ras_allele %in% alleles_to_use)
+
+    p <- plt_data %>%
+        ggplot(aes(ras_allele, contribution)) +
+        facet_wrap(~ signature, nrow = 1, scales = "free") +
+        geom_boxplot(aes(fill = ras_allele, color = ras_allele),
+                     alpha = 0.1, size = 0.5, outlier.shape = NA) +
+        geom_quasirandom(aes(color = ras_allele),
+                     data = plt_data %>% filter(contribution > 0),
+                     size = 0.4, varwidth = TRUE,
+                     groupOnX = TRUE, alpha = 0.4) +
+        geom_quasirandom(aes(color = ras_allele),
+                     data = plt_data %>% filter(contribution == 0),
+                     size = 0.4, varwidth = TRUE,
+                     groupOnX = TRUE, alpha = 0.4) +
+        scale_color_manual(values = short_allele_pal,
+                           drop = TRUE, guide = FALSE) +
+        scale_fill_manual(values = short_allele_pal,
+                          drop = TRUE, guide = FALSE) +
+        scale_y_continuous(expand = expansion(mult = c(0.01, 0.02))) +
+        theme_bw(base_size = 7, base_family = "Arial") +
+        theme(axis.title.x = element_blank(),
+              strip.background = element_blank(),
+              strip.text = element_text(size = 8, face = "bold")) +
+        labs(y = "contribution",
+             title = cancer)
+    plt_name <- as.character(glue("{cancer}_allele-sig-boxplots.svg"))
+    ggsave_wrapper(
+        p,
+        plot_path(GRAPHS_DIR, plt_name),
+        "wide"
+    )
+    saveFigRds(p, plt_name)
+    return(NULL)
+}
+
+mutsig_noartifact_df %>%
+    filter(!is_hypermutant) %>%
+    right_join(cancer_sigs_for_boxplot, by = c("cancer", "signature")) %>%
+    mutate(ras_allele = str_remove(ras_allele, "KRAS_"),
+           ras_allele = factor_alleles(ras_allele),
+           signature = factor_signatures(signature)) %>%
+    group_by(cancer) %>%
+    nest() %>%
+    mutate(a = walk2(cancer, data, plot_signature_boxplots))
