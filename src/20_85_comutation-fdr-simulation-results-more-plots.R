c7229da358a582f770a5e5b13586161b4aedee69
Josh Cook <joshuacook0023@gmail.com>
2020-09-06 09:29:45 -0400

    analysis: more plots for explaining comutation FDR simulation
    - I feel like a moron making these

--- src/20_85_comutation-fdr-simulation-results-more-plots.R
+++ src/20_85_comutation-fdr-simulation-results-more-plots.R
@@ -1,44 +1,44 @@
 
-library(tidyverse)
+# library(tidyverse)
 
-libs <- "stats, glue, conflicted, assertr, testthat,
-glmnet, parallel, caret, ggfortify, tidygraph, jhcutils,
-magrittr, ggpubr, ggraph, ggtext, patchwork, ggplot2, broom, tibble, magrittr,
-tidyverse"
-libs %>% str_split(",") %>% unlist() %>% str_squish() %>% walk(function(l) {
-    library(l, character.only = T)
-})
+# libs <- "stats, glue, conflicted, assertr, testthat,
+# glmnet, parallel, caret, ggfortify, tidygraph, jhcutils,
+# magrittr, ggpubr, ggraph, ggtext, patchwork, ggplot2, broom, tibble, magrittr,
+# tidyverse"
+# libs %>% str_split(",") %>% unlist() %>% str_squish() %>% walk(function(l) {
+#     library(l, character.only = T)
+# })
 
-library(mustashe)
+# library(mustashe)
 
-conflict_prefer("select", "dplyr")
-conflict_prefer("filter", "dplyr")
-conflict_prefer("slice", "dplyr")
-conflict_prefer("setdiff", "dplyr")
-conflict_prefer("intersect", "dplyr")
-conflict_prefer("cache", "ProjectTemplate")
-conflict_prefer("rename", "dplyr")
-conflict_prefer("parLapply", "parallel")
-conflict_prefer("which", "Matrix")
-
-
-for (f in list.files("lib", full.names = TRUE, pattern = "R$")) {
-    if (f == "lib/globals.R") { next }
-    tryCatch({
-        source(f)
-    }, error = function(e) {
-        message(glue("error in {f}: {e}"))
-    })
-
-}
+# conflict_prefer("select", "dplyr")
+# conflict_prefer("filter", "dplyr")
+# conflict_prefer("slice", "dplyr")
+# conflict_prefer("setdiff", "dplyr")
+# conflict_prefer("intersect", "dplyr")
+# conflict_prefer("cache", "ProjectTemplate")
+# conflict_prefer("rename", "dplyr")
+# conflict_prefer("parLapply", "parallel")
+# conflict_prefer("which", "Matrix")
 
 
-stash("comutation_fdr_simulation_results", {
-    load("cache/comutation_fdr_simulation_results.RData")
+# for (f in list.files("lib", full.names = TRUE, pattern = "R$")) {
+#     if (f == "lib/globals.R") { next }
+#     tryCatch({
+#         source(f)
+#     }, error = function(e) {
+#         message(glue("error in {f}: {e}"))
+#     })
 
-    comutation_fdr_simulation_results <- comutation_fdr_simulation_results %>%
-        filter(num_tumor_samples %in% c(100, 500, 1000, 3000))
-})
+# }
+
+
+# stash("comutation_fdr_simulation_results", {
+#     load("cache/comutation_fdr_simulation_results.RData")
+
+#     comutation_fdr_simulation_results <- comutation_fdr_simulation_results %>%
+#         filter(num_tumor_samples %in% c(100, 500, 1000, 3000))
+# })
 
 
 
@@ -135,3 +135,151 @@ demo_falsepos_grid_plot <- demo_params_results %>%
 ggsave_wrapper(demo_falsepos_grid_plot,
                plot_path(GRAPHS_DIR, "demo_falsepos-grid-plot.svg"),
                "small")
+
+
+odds_ratio_denisty <- demo_params_results %>%
+    ggplot(aes(x = estimate)) +
+    geom_density() +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.02))) +
+    theme(plot.title = element_text(hjust = 0.5),
+          plot.subtitle = element_text(hjust = 0.5)) +
+    labs(x = "odds ratio of simulation",
+         y = "density",
+         title = "Distribution of odds ratios from 1000 simulations",
+         subtitle = glue("Simulation of {demo_num_tumor_samples} tumor samples and lambdas of {demo_g1_rate} and {demo_g2_rate}."))
+ggsave_wrapper(odds_ratio_denisty,
+               plot_path(GRAPHS_DIR, "demo_odds-ratio-denisty.svg"),
+               "small")
+
+pvalue_denisty <- demo_params_results %>%
+    ggplot(aes(x = p_value)) +
+    geom_density() +
+    geom_vline(xintercept = 0.01, color = "blue", lty = 2) +
+    annotate("text", x = 0.04, y = 1.1,
+             label = "p-value = 0.01", hjust = 0,
+             color = "blue", family = "Arial", size = 3) +
+    scale_x_continuous(expand = c(0, 0), limits = c(0, 1.0)) +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.02))) +
+    theme(plot.title = element_text(hjust = 0.5),
+          plot.subtitle = element_text(hjust = 0.5)) +
+    labs(x = "p-value of simulation",
+         y = "density",
+         title = "Distribution of p-values from 1000 simulations",
+         subtitle = glue("Simulation of {demo_num_tumor_samples} tumor samples and lambdas of {demo_g1_rate} and {demo_g2_rate}."))
+ggsave_wrapper(pvalue_denisty,
+               plot_path(GRAPHS_DIR, "demo_pvalue-denisty.svg"),
+               "small")
+
+odds_pval_patch <- odds_ratio_denisty | pvalue_denisty
+ggsave_wrapper(odds_pval_patch,
+               plot_path(GRAPHS_DIR, "demo_odds-pval-patch.svg"),
+               width = 6, height = 3)
+
+
+oddsratio_denisty_plt <- comutation_fdr_simulation_results %>%
+    filter(near(g1_rate, demo_g1_rate) & near(g2_rate, demo_g2_rate)) %>%
+    mutate(grp = fct_reorder(as.character(num_tumor_samples),
+                             num_tumor_samples)) %>%
+    ggplot(aes(x = estimate)) +
+    geom_density(aes(group = grp, color = num_tumor_samples)) +
+    geom_vline(xintercept = 1, size = 0.4, color = "grey25") +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.02))) +
+    scale_color_distiller(type = "div", palette = "PRGn") +
+    theme(legend.position = c(0.85, 0.15),
+          plot.title = element_text(hjust = 0.5),
+          plot.subtitle = element_text(hjust = 0.5),
+          legend.key.size = unit(3, "mm"),
+          legend.background = element_blank()) +
+    labs(x = "odds ratio of simulation",
+         y = "density",
+         color = "number of simulation\ntumor samples",
+         title = "Distribution of simulations with different numbers of tumor samples",
+         subtitle = glue("Results of simulations with lambdas {demo_g1_rate} and {demo_g2_rate}."))
+ggsave_wrapper(oddsratio_denisty_plt,
+               plot_path(GRAPHS_DIR, "sim_oddsratio-density-tumorsamples.svg"),
+               "small")
+
+oddsratio_summary_scatter <- comutation_fdr_simulation_results %>%
+    filter(near(g1_rate, demo_g1_rate) & near(g2_rate, demo_g2_rate)) %>%
+    group_by(num_tumor_samples) %>%
+    summarise(avg_odds_ratio = median(estimate),
+              sd_odds_ratio = sd(estimate, na.rm = TRUE)) %>%
+    ungroup() %>%
+    mutate(or_upper = avg_odds_ratio + sd_odds_ratio,
+           or_lower = avg_odds_ratio - sd_odds_ratio) %>%
+    ggplot(aes(x = num_tumor_samples, y = avg_odds_ratio)) +
+    geom_point() +
+    geom_hline(yintercept = 1, size = 0.5, color = "grey25") +
+    geom_linerange(aes(ymin = avg_odds_ratio),
+                   size = 0.4, ymax = 1, color = "blue", alpha = 0.5) +
+    theme(plot.background = element_rect(fill = "white", color = "black")) +
+    labs(x = "number of tumor samples",
+         y = "avg. odds ratio")
+
+
+oddsratio_patch_layout <- c(
+    area(t = 1, l = 1, b = 10, r = 10),
+    area(t = 2, l = 6, b = 5, r = 9)
+)
+
+oddsratio_patch <- (oddsratio_denisty_plt + oddsratio_summary_scatter) +
+    plot_layout(design = oddsratio_patch_layout)
+ggsave_wrapper(oddsratio_patch,
+               plot_path(GRAPHS_DIR, "sim_oddsratio-tumorsamples_patch.svg"),
+               "small")
+
+
+# comutation_fdr_simulation_results %>%
+#     filter(near(g1_rate, demo_g1_rate) & near(g2_rate, demo_g2_rate)) %>%
+#     mutate(is_proj_sig = map2_lgl(mutation_table, p_value,
+#                                   project_significance_check)) %>%
+#     group_by(num_tumor_samples) %>%
+#     summarise(fdr = mean(is_proj_sig)) %>%
+#     ungroup() %>%
+#     ggplot(aes(x = num_tumor_samples, y = fdr)) +
+#     geom_line(group = "a") +
+#     geom_point(aes(color = fdr)) +
+#     scale_color_gradient(low = "skyblue", high = "blue") +
+#     labs(x = "number of tumor samples in simulation",
+#          y = "FDR",
+#          color = "FDR",
+#          title = "False discovery rate as the number of tumor samples increases")
+
+
+comutation_fdr_summary <- comutation_fdr_simulation_results %>%
+    mutate(is_proj_sig = map2_lgl(mutation_table, p_value,
+                                  project_significance_check),
+           grp = paste(g1_rate, g2_rate, sep = "_")) %>%
+    group_by(grp, num_tumor_samples, g1_rate, g2_rate) %>%
+    summarise(fdr = mean(is_proj_sig)) %>%
+    ungroup() %>%
+    mutate(max_mut_rate = map2_dbl(g1_rate, g2_rate, ~ max(.x, .y)))
+
+comutation_fdr_trend <- comutation_fdr_summary %>%
+    group_by(num_tumor_samples) %>%
+    summarise(avg_fdr = mean(fdr),
+              sd_fdr = sd(fdr)) %>%
+    ungroup()
+
+comutation_fdr_lineplt <- comutation_fdr_summary %>%
+    ggplot(aes(x = num_tumor_samples, y = fdr)) +
+    geom_line(aes(color = max_mut_rate, group = grp),
+              size = 0.5, alpha = 0.2) +
+    geom_line(aes(x = num_tumor_samples, y = avg_fdr),
+              data = comutation_fdr_trend,
+              size = 1, color = "black") +
+    scale_color_gradient(low = "dodgerblue", high = "tomato") +
+    scale_x_continuous(expand = expansion(mult = c(0, 0))) +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.01))) +
+    theme(plot.title = element_text(hjust = 0.5),
+          plot.subtitle = element_text(hjust = 0.5)) +
+    labs(x = "number of tumor samples in simulation",
+         y = "FDR",
+         color = "max mutation rate",
+         title = "False discovery rate as the number of tumor samples increases",
+         subtitle = "Each line is a simulation using different rates of mutation for the genes.")
+ggsave_wrapper(comutation_fdr_lineplt,
+               plot_path(GRAPHS_DIR, "comutation-fdr-lineplt.svg"),
+               "wide")
