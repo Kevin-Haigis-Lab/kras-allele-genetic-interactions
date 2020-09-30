cd5b26d3328e3b75b5ee80f30e32b2a12d6267e0
Josh Cook <joshuacook0023@gmail.com>
2020-09-30 09:16:40 -0400

    feat: more informative summary of number of lines of code function

--- lib/random.R
+++ lib/random.R
@@ -1,30 +1,58 @@
 
-# Count the number of lines with text in all the files in a directory.
-line_count_per_directory <- function(directory) {
-  counter <- 0
-  for (file in list.files(directory, full.names = TRUE, pattern = "R$")) {
-    all_lines <- readLines(file, warn = FALSE) %>%
-      unlist() %>%
-      str_trim()
-    all_lines <- all_lines[str_length(all_lines) > 0]
-    comment_line_idx <- str_detect(all_lines, "^#")
-    all_lines <- all_lines[!comment_line_idx]
-    counter <- counter + length(all_lines)
-  }
-  return(counter)
+# Count the number of lines of code in a file.
+line_count_per_file <- function(f) {
+  all_lines <- readLines(f, warn = FALSE) %>%
+    unlist() %>%
+    str_trim()
+  all_lines <- all_lines[str_length(all_lines) > 0]
+  comment_line_idx <- str_detect(all_lines, "^#")
+  all_lines <- all_lines[!comment_line_idx]
+  return(length(all_lines))
 }
 
 
+list_files_in_dir <- function(dir) {
+  list.files(dir, full.names = TRUE, pattern = "R$|py$|sh$")
+}
 
 # The number of lines of code in this project
 comutation_proj_lines_of_code <- function() {
-  lapply(
-    c(
-      "src", "munge", "lib", "tests",
+
+  proj_dirs <- c(
+      "src",
+      "munge",
+      "lib",
+      "tests",
       file.path("paper", "figures", "make_figure_scripts")
-    ),
-    line_count_per_directory
-  ) %>%
-    unlist() %>%
-    sum()
+    )
+
+  d <- tibble(dir = proj_dirs) %>%
+    mutate(file = map(dir, list_files_in_dir)) %>%
+    unnest(file) %>%
+    mutate(num_lines = map_dbl(file, line_count_per_file))
+
+  summ_d <- d %>%
+    group_by(dir) %>%
+    summarise(
+      n_files = n_distinct(file),
+      min = min(num_lines),
+      q25 = quantile(num_lines, 0.25),
+      mean = mean(num_lines),
+      median = median(num_lines),
+      q75 = quantile(num_lines, 0.75),
+      max = max(num_lines),
+      total = sum(num_lines)
+    ) %>%
+    ungroup() %>%
+    mutate(dir = basename(dir))
+
+  summ_d %>%
+    knitr::kable(digits = 0) %>%
+    print()
+  cat("\n")
+  num_files <- nrow(d)
+  num_lines <- sum(d$num_lines)
+  message(glue("#> There are {num_lines} lines of code in {num_files} files."))
 }
+
+comutation_proj_lines_of_code()
