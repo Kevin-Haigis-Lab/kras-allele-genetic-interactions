f0dd1774232c6b9f4d0573e0384dcc7c3209f585
Josh Cook <joshuacook0023@gmail.com>
2020-12-08 16:28:15 -0500

    analysis: DUSP diff. expr. by APC mutation - also remove excess from "_temp....Rmd"

--- munge/27_cancer-rna-expression.R
+++ munge/27_cancer-rna-expression.R
@@ -33,4 +33,3 @@ cache("tcga_coad_rna", {
     )
   }
 })
-
--- src/90_44_yijang_dusp-analysis.R
+++ src/90_44_yijang_dusp-analysis.R
@@ -10,18 +10,19 @@ reset_table_directory(TABLES_DIR)
 #### ---- Comutation interaction list ---- ####
 
 
-nonallele_specific_increased_comutation_df %.% {
-  filter(cancer == "COAD" & hugo_symbol != "KRAS")
-  filter(p_value < 0.01)
-  arrange(p_value)
-  mutate(
-    geneWT_krasWT = map_dbl(comut_ct_tbl, ~ .x[1, 1]),
-    geneMut_krasWT = map_dbl(comut_ct_tbl, ~ .x[2, 1]),
-    geneWT_krasMut = map_dbl(comut_ct_tbl, ~ .x[1, 2]),
-    geneMut_krasMut = map_dbl(comut_ct_tbl, ~ .x[2, 2]),
-  )
-  select(hugo_symbol, p_value, odds_ratio, tidyselect::starts_with("gene"))
-} %>%
+nonallele_specific_increased_comutation_df %.%
+  {
+    filter(cancer == "COAD" & hugo_symbol != "KRAS")
+    filter(p_value < 0.01)
+    arrange(p_value)
+    mutate(
+      geneWT_krasWT = map_dbl(comut_ct_tbl, ~ .x[1, 1]),
+      geneMut_krasWT = map_dbl(comut_ct_tbl, ~ .x[2, 1]),
+      geneWT_krasMut = map_dbl(comut_ct_tbl, ~ .x[1, 2]),
+      geneMut_krasMut = map_dbl(comut_ct_tbl, ~ .x[2, 2]),
+    )
+    select(hugo_symbol, p_value, odds_ratio, tidyselect::starts_with("gene"))
+  } %>%
   write_tsv(table_path(TABLES_DIR, "comutation-list.tsv"))
 
 
@@ -29,5 +30,9 @@ nonallele_specific_increased_comutation_df %.% {
 #### ---- RNA expression data ---- ####
 
 tcga_coad_rna %>%
-  filter(str_detect(hugo_symbol, "DUSP")) %>%
-  saveRDS("coad-dusp-rna_expression.rds")
+  filter(str_detect(hugo_symbol, "DUSP"))
+
+cancer_full_coding_muts_df %>%
+  filter(hugo_symbol == "APC" & cancer == "COAD") %>%
+  distinct(tumor_sample_barcode, hugo_symbol, mutation_type, amino_acid_change) %T>%
+  saveRDS("coad-apc-mutations.rds")
