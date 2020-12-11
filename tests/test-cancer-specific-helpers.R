2e9886f19886d499ebe3b11ae554e48b714117f3
Josh Cook <joshuacook0023@gmail.com>
2020-12-11 09:13:01 -0500

    test: ggplot2 helpers

--- tests/test-cancer-specific-helpers.R
+++ tests/test-cancer-specific-helpers.R
@@ -3,14 +3,9 @@ cancer_names <- c("COAD", "LUAD", "PAAD", "MM", "SKCM")
 
 # test `get_hugo_from_depmap_ids()`
 test_that("Cancer specific named vectors contain valid cancers.", {
-
-  compare_against_cancer_names <- function(x) {
-    expect_true(all(names(x) %in% cancer_names))
-  }
-
-  compare_against_cancer_names(cancer_oncogenes)
-  compare_against_cancer_names(FRACTION_OF_TISSUE_THAT_ARE_CANCER)
-  compare_against_cancer_names(cgc_cancer_regex)
+  expect_true(all(names(cancer_oncogenes) %in% cancer_names))
+  expect_true(all(names(FRACTION_OF_TISSUE_THAT_ARE_CANCER) %in% cancer_names))
+  expect_true(all(names(cgc_cancer_regex) %in% cancer_names))
 })
 
 
@@ -20,5 +15,6 @@ test_that("The COSMIC CGC genes can be retrieved.", {
     genes <- get_cgc_genes(cancer)
     expect_true(all(is.character(genes)))
     expect_true(!any(is.na(genes)))
+    # expect_snapshot_value(get_cgc_genes(cancer))
   }
 })
