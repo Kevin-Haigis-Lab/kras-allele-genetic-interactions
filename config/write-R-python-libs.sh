dac6db7d7d2bbc00fe5fe9d4d287f2457e63ffa3
Josh Cook <joshuacook0023@gmail.com>
2021-02-11 10:27:57 -0500

    feat: use renv to organize and track R packages

--- config/write-R-python-libs.sh
+++ config/write-R-python-libs.sh
@@ -4,14 +4,16 @@
 
 module load gcc R/4.0.1 conda2/4.2.13
 
+source ~/.bashrc
+
 # Output files.
 R_LIB_FILE="config/R-libraries.txt"
 PY_LIB_FILE="config/python-env.yaml"
+
+# Take a snapshot of the library using 'renv'.
+Rscript -e "renv::snapshot()"
 
 # Export conda environment.
 source activate rctest
 conda env export > $PY_LIB_FILE
 conda deactivate 
-
-# Make a text file listing all R libraries.
-Rscript -e "source('lib/make-project-helpers.R');write_package_list('$R_LIB_FILE')"
